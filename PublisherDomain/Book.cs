cfa93e86128efe7aa5dbd1e76e51bc154c2582dc
Kautsar Rahman <kautsar.rahman@gmail.com>
2026-04-25 11:50:28 +0900

    Initial Setup

---
+++ PublisherDomain/Book.cs
@@ -0,0 +1,13 @@
+﻿namespace PublisherDomain
+{
+    public class Book
+    {
+        public int BookId { get; set; }
+        public string Title { get; set; }
+        public DateTime PublisherDate { get; set; }
+        public decimal BasePrice { get; set; }
+        public Author Author { get; set; }
+        public int AuthorId { get; set; }
+
+    }
+}
