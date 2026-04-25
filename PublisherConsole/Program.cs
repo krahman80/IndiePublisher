61e0a54d2a7dfced753d3a93ce30cc98dfc43161
Kautsar Rahman <kautsar.rahman@gmail.com>
2026-04-25 18:15:47 +0900

    Reading and Writing Some Data

--- PublisherConsole/Program.cs
+++ PublisherConsole/Program.cs
@@ -1,3 +1,31 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using PublisherData;
+using PublisherDomain;
 
+// Generate Database on the fly
+using (PubContext context = new PubContext())
+{
+    context.Database.EnsureCreated();
+}
+
+GetAuthors();
+AddAuthor();
+GetAuthors();
+
+void AddAuthor()
+{
+    var author = new Author { FirstName = "Josie", LastName = "Newf" };
+    using var context = new PubContext();
+    context.Authors.Add(author);
+    context.SaveChanges();
+}
+
+void GetAuthors()
+{
+    using var context = new PubContext();
+    var authors = context.Authors.ToList();
+    foreach (var author in authors)
+    {
+        Console.WriteLine(author.FirstName + " " + author.LastName);
+    }
+}
