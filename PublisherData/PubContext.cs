a1dcae2ced136f3da85a73be65eced5b978eca18
Kautsar Rahman <kautsar.rahman@gmail.com>
2026-04-25 17:50:28 +0900

    Add PublisherData Project

---
+++ PublisherData/PubContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PublisherDomain;
+
+namespace PublisherData;
+public class PubContext:DbContext
+{
+    public DbSet<Author> Authors { get; set; }
+    public DbSet<Book> Books { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlite("Data Source=PublisherData.db");
+    }
+
+}
+
