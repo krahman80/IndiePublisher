88f03efc9b6eae9abc9f27e7070d29c97dbbd0df
Kautsar Rahman <kautsar.rahman@gmail.com>
2026-05-08 17:09:37 +0900

    Many to Many Relation Using Skip Navigation, New Migration, DB update

---
+++ PublisherData/Migrations/20260508072302_ArtistAndCover.cs
@@ -0,0 +1,85 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PublisherData.Migrations
+{
+    public partial class ArtistAndCover : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Artists",
+                columns: table => new
+                {
+                    ArtistId = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    FirstName = table.Column<string>(type: "TEXT", nullable: false),
+                    LastName = table.Column<string>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Artists", x => x.ArtistId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Covers",
+                columns: table => new
+                {
+                    CoverId = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    DesignIdeas = table.Column<string>(type: "TEXT", nullable: false),
+                    DigitalOnly = table.Column<bool>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Covers", x => x.CoverId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ArtistCover",
+                columns: table => new
+                {
+                    ArtistsArtistId = table.Column<int>(type: "INTEGER", nullable: false),
+                    CoversCoverId = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ArtistCover", x => new { x.ArtistsArtistId, x.CoversCoverId });
+                    table.ForeignKey(
+                        name: "FK_ArtistCover_Artists_ArtistsArtistId",
+                        column: x => x.ArtistsArtistId,
+                        principalTable: "Artists",
+                        principalColumn: "ArtistId",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_ArtistCover_Covers_CoversCoverId",
+                        column: x => x.CoversCoverId,
+                        principalTable: "Covers",
+                        principalColumn: "CoverId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ArtistCover_CoversCoverId",
+                table: "ArtistCover",
+                column: "CoversCoverId");
+            migrationBuilder.CreateIndex(
+                name: "IX_ArtistCover_ArtistsArtistId",
+                table: "ArtistCover",
+                column: "ArtistsArtistId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ArtistCover");
+
+            migrationBuilder.DropTable(
+                name: "Artists");
+
+            migrationBuilder.DropTable(
+                name: "Covers");
+        }
+    }
+}
--- PublisherData/PubContext.cs
+++ PublisherData/PubContext.cs
@@ -8,6 +8,8 @@ public class PubContext:DbContext
 
     public DbSet<Author> Authors { get; set; }
     public DbSet<Book> Books { get; set; }
+    public DbSet<Artist> Artists { get; set; }
+    public DbSet<Cover> Covers { get; set; }
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
